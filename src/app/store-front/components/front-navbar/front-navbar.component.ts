8285b7d2283e5f2ff22c64e7bc1c029b2c511e4d
yunioseijo <yunioseijo@gmail.com>
2025-04-30 20:26:02 +0200

    add the navbar html structure

--- src/app/store-front/components/front-navbar/front-navbar.component.ts
+++ src/app/store-front/components/front-navbar/front-navbar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'front-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './front-navbar.component.html',
 })
 export class FrontNavbarComponent { }
