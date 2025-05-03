5d00628a3ef14446cbbdafc17a7851bb59e5547a
yunioseijo <yunioseijo@gmail.com>
2025-05-03 15:29:03 +0200

    feat: implement product image pipe for dynamic image loading and placeholder support

--- src/app/store-front/components/store-front-card/product-card.component.ts
+++ src/app/store-front/components/store-front-card/product-card.component.ts
@@ -2,10 +2,11 @@ import { SlicePipe } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Product } from '@products/interfaces/product.interface';
+import { ProductImagePipe } from '@products/pipes/product-image.pipe';
 
 @Component({
   selector: 'product-card',
-  imports: [RouterLink, SlicePipe],
+  imports: [RouterLink, SlicePipe, ProductImagePipe],
   templateUrl: './product-card.component.html',
 })
 export class ProductCardComponent {
