56de3e33dd87147b3c97488662246a7ae2cca78a
yunioseijo <yunioseijo@gmail.com>
2025-05-04 15:44:35 +0200

    feat: add Swiper.js for product carousel functionality with navigation and pagination

--- src/app/products/components/product-carousel/product-carousel.component.ts
+++ src/app/products/components/product-carousel/product-carousel.component.ts
@@ -1,10 +1,50 @@
-import { Component, input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, input, viewChild } from '@angular/core';
+// import Swiper JS
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { ProductImagePipe } from '@products/pipes/product-image.pipe';
 
 @Component({
   selector: 'product-carousel',
-  imports: [],
+  imports: [ProductImagePipe],
   templateUrl: './product-carousel.component.html',
+  styles: `
+    .swiper {
+      width: 100%;
+
+    }
+    `,
 })
-export class ProductCarouselComponent {
-  images = input.required<string[] | null>()
+export class ProductCarouselComponent implements AfterViewInit {
+  images = input.required<string[] | null>();
+
+  swiperDiv = viewChild.required<ElementRef>('swiperDiv');
+
+  ngAfterViewInit(): void {
+    const element = this.swiperDiv().nativeElement;
+    if (!element) return;
+    console.log('element', element);
+    console.log(this.images());
+    const swiper = new Swiper(element, {
+      // Optional parameters
+      direction: 'horizontal',
+      modules: [Navigation, Pagination],
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+
+
+  }
 }
