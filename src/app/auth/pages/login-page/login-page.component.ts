da324e1928187d243f627beb18fe48030ae3ad48
yunioseijo <yunioseijo@gmail.com>
2025-05-09 16:45:47 +0200

    feat: enhance login page with reactive form validation and error handling

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { AlertComponent } from '../../../shared/components/alert/alert.component';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [AlertComponent, RouterLink, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
 })
-export class LoginPageComponent { }
+export class LoginPageComponent {
+  fb = inject(FormBuilder);
+  hasError = signal(false);
+  isPosting = signal(false);
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit() {
+    if(this.loginForm.invalid){
+      this.hasError.set(true);
+      setTimeout(() => {
+        this.hasError.set(false);
+      }
+      , 3000);
+      return;
+    }
+    const {email = '', password = ''} = this.loginForm.value;
+    console.log({email, password});
+
+  }
+ }
---
+++ src/app/shared/components/alert/alert.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alert',
+  imports: [],
+  templateUrl: './alert.component.html',
+})
+export class AlertComponent { }
