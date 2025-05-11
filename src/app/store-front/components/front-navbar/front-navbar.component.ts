2fec6cb605b955f224c1f21bf37d2363590a49a5
yunioseijo <yunioseijo@gmail.com>
2025-05-11 21:03:01 +0200

    feat: enhance front navbar with user display and authentication status handling

--- src/app/store-front/components/front-navbar/front-navbar.component.ts
+++ src/app/store-front/components/front-navbar/front-navbar.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '@auth/services/auth.service';
 
 @Component({
   selector: 'front-navbar',
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './front-navbar.component.html',
 })
-export class FrontNavbarComponent { }
+export class FrontNavbarComponent {
+  authService = inject(AuthService);
+}
