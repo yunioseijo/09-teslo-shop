63b10678ba5de3ac9b8072551c6190dbedb2a683
yunioseijo <yunioseijo@gmail.com>
2025-05-12 07:43:37 +0200

    feat: implement registration functionality with form validation and error handling feat: add authentication interceptors for request handling feat: integrate NotAuthenticatedGuard for route protection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,19 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { loggingInterceptor } from '@shared/interceptors/logging.interceptor';
+import { authInterceptor } from '@auth/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withFetch()),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([
+        // loggingInterceptor,
+        authInterceptor]),
+    ),
   ]
 };
